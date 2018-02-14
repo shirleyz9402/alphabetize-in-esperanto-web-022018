a68b9b3051826570153e58bbe2e442841a394509
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-14 00:39:43 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
 def alphabetize(arr)
-  # code here
+ alpha_hash = {}
+ esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ alphabet = esperanto_alphabet.split("")
+ alphabet.each_with_index{|item,index| alpha_hash[item] = index}
+  arr.each do |element|
+    element.split("").each do |letter|
+      letter = alpha_hash[letter]
 end
